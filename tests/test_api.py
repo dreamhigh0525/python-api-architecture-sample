371de00cfa571a304874174e7f2042779107a643
dreamhigh0525 <dreamhigh0525@gmail.com>
2020-10-18 18:34:29 +0900

    added test sample

---
+++ tests/test_api.py
@@ -0,0 +1,25 @@
+import pytest
+import app
+
+
+@pytest.fixture
+def api():
+    return app.api
+
+
+def test_inference(api):
+    filename = 'test/test_image.jpg'
+    image_file = open(filename, 'rb').read()
+    files = {
+        'id': (None, 'test id', 'text/plain'),
+        'file': (filename, image_file, 'image/jpeg'),
+        'category': (None, 'movie', 'text/plain'),
+        'url': (None, 'https://localhost/test.jpg', 'text/plain')
+    }
+    r = api.requests.post('/inference', files=files)
+    assert r.status_code == 202
+
+
+def test_report(api):
+    r = api.requests.get('/report')
+    assert r.status_code == 200
