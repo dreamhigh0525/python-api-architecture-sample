b1996cdcbf6dda53624d1bafd06181b48a16be0c
dreamhigh0525 <dreamhigh0525@gmail.com>
2020-10-20 01:01:17 +0900

    modified app runner setting

--- src/bin/app.py
+++ src/bin/app.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import os
+
+import uvicorn
 from inference.helper.api_module import api
 from inference.application.inference_controller import InferenceController
 from inference.application.exception import exception_handler
 
-controller = InferenceController()
-api.add_route('/inference', controller.on_post)
-api.add_route('/report', controller.on_get)
-api.add_route('/', exception_handler, default=True)
 
-if __name__ == '__main__':
+def main():
+    controller = InferenceController()
+    api.add_route('/inference', controller.on_post)
+    api.add_route('/report', controller.on_get)
+    api.add_route('/', exception_handler, default=True)
     port = os.environ.get('PORT', 80)
     debug = os.environ.get('DEBUG', False)
-    api.run(port=port, debug=debug)
+    api.run(address="0.0.0.0", port=port, debug=debug)
+
+
+if __name__ == '__main__':
+    main()
+    #port = os.environ.get('PORT', 80)
+    #debug = os.environ.get('DEBUG', False)
+    #api.run(address="0.0.0.0", port=port, debug=debug)
+    #uvicorn.run(api, host="0.0.0.0", port=port)
