fcb5ecd4dd1470659e245ce0866a4acf761f7796
dreamhigh0525 <dreamhigh0525@gmail.com>
2020-10-25 23:42:34 +0900

    modified logger settings

--- src/inference/helper/api_module.py
+++ src/inference/helper/api_module.py
@@ -1,6 +1,7 @@
 
 import responder
-from logging import getLogger, Formatter, FileHandler, StreamHandler, INFO
+from logging import getLogger, Formatter, StreamHandler, INFO
+from logging.handlers import TimedRotatingFileHandler
 
 api = responder.API(
     openapi='3.0.0',
@@ -9,9 +10,15 @@ api = responder.API(
     version='0.0.1'
 )
 
+
 logger = getLogger(__name__)
 formatter = Formatter('%(asctime)s [%(levelname)s] :%(message)s')
-file_handler = FileHandler('request.log')
+file_handler = TimedRotatingFileHandler(
+    filename='access.log',
+    when='midnight',
+    backupCount=7,
+    encoding='utf-8'
+)
 file_handler.setLevel(INFO)
 file_handler.setFormatter(formatter)
 stream_handler = StreamHandler()
@@ -19,6 +26,6 @@ stream_handler.setLevel(INFO)
 stream_handler.setFormatter(formatter)
 
 logger.setLevel(INFO)
-# logger.addHandler(file_handler)
+logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 logger.propagate = False
