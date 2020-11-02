fcc28b8f9e74abd4bc7fb03b34973af4de975ce9
dreamhigh0525 <dreamhigh0525@gmail.com>
2020-11-02 17:15:23 +0900

    added service test case

---
+++ tests/domain/test_report_service.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+import pytest
+from unittest import mock
+from inference.domain.object.content import Content
+from inference.domain.object.inference import Inference
+from inference.domain.service.report_service import ReportService
+
+
+class TestReportService:
+
+    @pytest.fixture(scope='module')
+    def result(mocker: mock) -> Tuple[Content, Inference]:
+        with open('tests/test_image.jpg', 'rb') as f:
+            image_data = f.read()
+        result = (
+            Content('test id', 'movie', 'http://localhost', image_data),
+            Inference('0', 0.99)
+        )
+        return result
+
+    def test_report_inference(self, mocker: mock, result: Tuple[Content, Inference]):
+        repository_mock = mocker.MagicMock()
+        repository_mock.report_inference = mocker.Mock(return_value=None)
+        service = ReportService(repository_mock)
+        service.report_inference(result[0], result[1])
+        repository_mock.report_inference.assert_called_once_with(result[0], result[1])
